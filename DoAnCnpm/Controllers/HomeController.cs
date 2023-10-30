74c8e850b68d1befa260ab696bab7029f211d8d5
doantam123 <doantampn2017@gmail.com>
2023-10-30 10:35:27 +0700

    update trang home

--- DoAnCnpm/Controllers/HomeController.cs
+++ DoAnCnpm/Controllers/HomeController.cs
@@ -30,56 +30,61 @@ namespace DoAnCnpm.Controllers
             return View();
         }
 
-        public ActionResult ITestGD()
-        {
-            List<OrderDetail> orderDetail = context.OrderDetails.ToList();
-            List<Product> proList = context.Products.ToList();
+        //public ActionResult ITestGD()
+        //{
+        //    List<OrderDetail> orderDetail = context.OrderDetails.ToList();
+        //    List<Product> proList = context.Products.ToList();
 
-            var query = from od in orderDetail
-                        join p in proList on od.IDProduct equals p.ProductID into tbl
-                        group od by new
-                        {
-                            idPro = od.IDProduct,
-                            namePro = od.Product.NamePro,
-                            imagePro = od.Product.ImagePro,
-                            price = od.Product.Price
-                        }
-                        into gr
-                        orderby gr.Sum(s => s.Quantity) descending
-                        select new ViewModel
-                        {
-                            IDPro = gr.Key.idPro,
-                            NamePro = gr.Key.namePro,
-                            ImgPro = gr.Key.imagePro,
-                            pricePro = (decimal)gr.Key.price,
-                            sum_Quantity = gr.Sum(s => s.Quantity)
-                        };
-            return View(query.Take(4).ToList());
-        }
+        //    var query = from od in orderDetail
+        //                join p in proList on od.IDProduct equals p.ProductID into tbl
+        //                group od by new
+        //                {
+        //                    idPro = od.IDProduct,
+        //                    namePro = od.Product.NamePro,
+        //                    imagePro = od.Product.ImagePro,
+        //                    price = od.Product.Price
+        //                }
+        //                into gr
+        //                orderby gr.Sum(s => s.Quantity) descending
+        //                select new ViewModel
+        //                {
+        //                    IDPro = gr.Key.idPro,
+        //                    NamePro = gr.Key.namePro,
+        //                    ImgPro = gr.Key.imagePro,
+        //                    pricePro = (decimal)gr.Key.price,
+        //                    sum_Quantity = gr.Sum(s => s.Quantity)
+        //                };
+        //    return View(query.Take(4).ToList());
+        //}
         [AllowAnonymous]
         public ActionResult TestAD()
         {
             return View();
         }
 
-     /*   public ActionResult GetData()
+      public ActionResult GetData()
         {
-          *//*  int thang6 = context.HighCharts.Where(x => x.ThoiGian == "Thang6").Count();
-            int thang7 = context.HighCharts.Where(x => x.ThoiGian == "Thang7").Count();
-            int thang8 = context.HighCharts.Where(x => x.ThoiGian == "Thang8").Count();*//*
+            int thang8 = context.HighCharts.Where(x => x.ThoiGian == "Thang8").Count();
+            int thang9 = context.HighCharts.Where(x => x.ThoiGian == "Thang9").Count();
+            int thang10 = context.HighCharts.Where(x => x.ThoiGian == "Thang10").Count();
             Ratio obj = new Ratio();
-            obj.Thang6 = thang6;
-            obj.Thang7 = thang7;
-            obj.Thang8 = thang8;*//*
+            obj.Thang8 = thang8;
+            obj.Thang9 = thang9;
+            obj.Thang10 = thang10;
 
-            return Json(obj, JsonRequestBehavior.AllowGet);*//*
-        }*/
+            return Json(obj, JsonRequestBehavior.AllowGet);
+        }
         public class Ratio
         {
-            public int Thang6 { get; set; }
-            public int Thang7 { get; set; }
             public int Thang8 { get; set; }
+            public int Thang9 { get; set; }
+            public int Thang10 { get; set; }
         }
+
+
+        public ActionResult TrangHome() {
+            return View();
+                }
 
     }
 }
